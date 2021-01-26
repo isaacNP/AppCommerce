3a21f04686cf072e00c4558218b9af4ec0765e15
isaac <isaac.npcad@gmail.com>
2021-01-26 15:02:04 -0300

    Etapa 3 - Design da Tela Produto
    Trabalhando com Scroll View

---
+++ app/src/main/java/br/com/isaac/appcommerce/ProductDetailActivity.kt
@@ -0,0 +1,14 @@
+package br.com.isaac.appcommerce
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
